a328d71957dfb34db46faa007ebe3dec685005b5
bkpatel3108 <bkpatel3108@gmail.com>
2018-10-08 23:10:17 -0400

    add lc209

---
+++ algorithmsplus/src/main/java/me/algo/leetcode/Lc209MinimumSizeSubarraySum.java
@@ -0,0 +1,25 @@
+package me.algo.leetcode;
+
+public class Lc209MinimumSizeSubarraySum {
+	public int minSubArrayLen(int s, int[] nums) {
+		if (nums == null || nums.length == 0)
+			return 0;
+		// sliding window start
+		int start = 0;
+		// sliding window end
+		int end = 0;
+		int currSum = 0;
+		int min = Integer.MAX_VALUE;
+		while (end < nums.length) {
+			currSum += nums[end];
+			while (currSum >= s) {
+				min = Math.min(end - start + 1, min);
+				currSum -= nums[start];
+				start++;
+			}
+			end++;
+		}
+
+		return min == Integer.MAX_VALUE ? 0 : min;
+	}
+}
